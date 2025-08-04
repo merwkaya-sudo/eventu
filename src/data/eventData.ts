7e79e088c3a9565ba76cccac97c7316e17c5ef2d
mervekaya <mervekaya@mervekayas-MacBook-Air.local>
2025-08-04 15:42:40 +0300

    Update event titles and images from Luma

--- src/data/eventData.ts
+++ src/data/eventData.ts
@@ -18,7 +18,7 @@ export const eventData: EventData[] = [
     location: "Online",
     category: 'AI',
     attendees: 500,
-    image: 'https://images.pexels.com/photos/8386440/pexels-photo-8386440.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://images.ctfassets.net/b7nkce0x59hz/3c2wbiDuPudUm2kjRNTMIp/87be90c6fd51e9b6b1d8e6b308847623/AI_Meetup_-_Microsoft_Software_Engineer.png?w=1920&q=50',
     isOnline: true,
     url: "https://lu.ma/k8bp2p3a"
   },
@@ -29,7 +29,7 @@ export const eventData: EventData[] = [
     location: "Online",
     category: 'PM',
     attendees: 300,
-    image: 'https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://images.ctfassets.net/b7nkce0x59hz/6nM6CH8ZnqAI2zpPVLhFQY/acc0b3c05fc61efac763d2ea7b8c02a6/hasan-soylemez.png?w=1920&q=50',
     isOnline: true,
     url: "https://lu.ma/tpgtml47"
   },
@@ -38,9 +38,9 @@ export const eventData: EventData[] = [
     title: "DIY AI – Own Your Intelligence",
     date: "2024-08-28",
     location: "Online",
-    category: 'Create Image',
+    category: 'AI Tools',
     attendees: 250,
-    image: 'https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://images.ctfassets.net/b7nkce0x59hz/7jjqbYaa0Ao9j3XKrvlbnd/f6b9d1ab6e946c03fc96d0c96973f31b/diy-ai-own-intelligence.png?w=1920&q=50',
     isOnline: true,
     url: "https://lu.ma/xexdptms"
   },
@@ -51,7 +51,7 @@ export const eventData: EventData[] = [
     location: "Online",
     category: 'Design',
     attendees: 400,
-    image: 'https://images.pexels.com/photos/8386434/pexels-photo-8386434.jpeg?auto=compress&cs=tinysrgb&w=800',
+    image: 'https://images.ctfassets.net/b7nkce0x59hz/38FlJ9Fdu59EuJ0tmyZ5oF/fc3b3d96dcfd3b90d8b5a3a39a0f9323/novus-product-design.png?w=1920&q=50',
     isOnline: true,
     url: "https://lu.ma/tla76mad"
   }
